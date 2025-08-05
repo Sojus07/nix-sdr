9648aa823daf3a97e0d7c82b69794598421a5e53
Sojus07 <Sojus07>
2025-08-05 18:15:57 +0200

    add gnss-sdr as package

--- flake.nix
+++ flake.nix
@@ -3,7 +3,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {
+      gnss-sdr = import ./gnss-sdr.nix { inherit pkgs; };
+    };
+
     nixosModules.default = import ./sdr.nix;
   };
 }
